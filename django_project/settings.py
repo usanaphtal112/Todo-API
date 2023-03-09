fce23ee314bf9e997be1082c53d4c808cd1986b5
usanaphtal112 <usanaphtal112@gmail.com>
2023-03-09 12:10:19 +0200

    Initial commit, Todo-list API

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,19 +37,38 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # 3rd party
+    "rest_framework",  # new
+    "corsheaders",  # new
     # Local apps
     "todos.apps.TodosConfig",  # New
 ]
 
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
+    ],
+}
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",  # new
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+CORS_ALLOWED_ORIGINS = (
+    "http://localhost:3000",
+    "http://localhost:8000",
+)
+
+CSRF_TRUSTED_ORIGINS = ["localhost:3000"]
+
 
 ROOT_URLCONF = "django_project.urls"
 
--- todos/tests.py
+++ todos/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from .models import Todo
+from django.test import TestCase
+from django.urls import reverse  # new
+from rest_framework import status  # new
+from rest_framework.test import APITestCase  # new
+
+
+class TodoModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.todo = Todo.objects.create(
+            title="This is test title", body="This is test body"
+        )
+
+    def test_model_content(self):
+        self.assertEqual(self.todo.title, "This is test title")
+        self.assertEqual(self.todo.body, "This is test body")
+        self.assertEqual(str(self.todo), "This is test title")
+
+    def test_api_listview(self):  # new
+        response = self.client.get(reverse("todo_list"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Todo.objects.count(), 1)
+        self.assertContains(response, self.todo)
+
+    def test_api_detailview(self):  # new
+        response = self.client.get(
+            reverse("todo_detail", kwargs={"pk": self.todo.id}), format="json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Todo.objects.count(), 1)
+        self.assertContains(response, "First Todo")
